64879d528de211b8652eb461a354e49ff379e57b
LakshyaChauhan <clakshya923@gmail.com>
2023-06-23 12:38:22 +0530

    nothing

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,47 +1,48 @@
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
+import 'package:mealsapp/data/dummy_data.dart';
+import 'package:mealsapp/models/category.dart';
+import 'package:mealsapp/screens/meals.dart';
+import 'package:mealsapp/widgets/categorygriditem.dart';
 
 class CategoryScreen extends StatelessWidget {
   const CategoryScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredList =
+        dummyMeals.where((meal) => meal.categories.contains(category.id));
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (ctx) => MealsScreen(
+                title: category.title, meals: filteredList.toList())));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          centerTitle: true,
           forceMaterialTransparency: true,
           titleTextStyle: GoogleFonts.aldrich(fontSize: 20),
-          title: const Text('Meals App'),
+          title: const Text('Pick your Category'),
         ),
         body: GridView(
+          padding: const EdgeInsets.all(16),
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 2,
               childAspectRatio: 1.5,
               crossAxisSpacing: 20,
               mainAxisSpacing: 20),
-          children: const [
-            Text(
-              '1',
-              style: TextStyle(color: Colors.white70),
-            ),
-            Text(
-              '2',
-              style: TextStyle(color: Colors.white70),
-            ),
-            Text(
-              '2',
-              style: TextStyle(color: Colors.white70),
-            ),
-            Text(
-              '4',
-              style: TextStyle(color: Colors.white70),
-            ),
-            Text(
-              '5',
-              style: TextStyle(color: Colors.white70),
-            ),
-          ],
+          children: availableCategories
+              .map((category) => CategoryGridItem(
+                    category: category,
+                    onSelectedCategory: () {
+                      
+                      _selectCategory(context, category);
+                    },
+                  ))
+              .toList(),
         ));
   }
 }
--- lib/widgets/categorygriditem.dart
+++ lib/widgets/categorygriditem.dart
@@ -1,14 +1,39 @@
-import 'package:flutter/foundation.dart';
+import 'package:mealsapp/models/category.dart';
 import 'package:flutter/material.dart';
-import 'package:mealsapp/screens/categories.dart';
 
 class CategoryGridItem extends StatelessWidget {
-  const CategoryGridItem({super.key, required this.category});
+  const CategoryGridItem(
+      {super.key, required this.category, required this.onSelectedCategory});
 
   final Category category;
+  final void Function() onSelectedCategory;
 
   @override
   Widget build(BuildContext context) {
-    return const CategoryScreen();
+    return InkWell(
+      onTap: onSelectedCategory,
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            gradient: LinearGradient(
+              colors: [
+                category.color.withOpacity(0.55),
+                category.color.withOpacity(0.9)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            )),
+        child: Text(
+          category.title,
+          style: Theme.of(context)
+              .textTheme
+              .titleLarge!
+              .copyWith(color: Theme.of(context).colorScheme.onBackground),
+        ),
+      ),
+    );
   }
 }
